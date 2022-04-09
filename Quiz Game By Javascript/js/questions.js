643e2591d73f67a2c814183631c207bc9357e67e
moinuddinvaidya786 <102137468+moinuddinvaidya786@users.noreply.github.com>
2022-04-09 17:03:48 +0530

    Add files via upload

---
+++ Quiz Game By Javascript/js/questions.js
@@ -0,0 +1,69 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which type of framework Reactjs is?",
+    answer: "Open-source frontend",
+    options: [
+      "Open-source frontend",
+      "Open-source backend",
+      "None of these",
+      "Both of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS Stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "Which tools from given below we used for frontend development?",
+    answer: "All of these",
+    options: [
+      "jQuery",
+      "Sublime Text",
+      "Sass",
+      "All of these"
+    ]
+  },
+];
